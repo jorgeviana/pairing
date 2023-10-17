cbe504b520f0cc33e92b3cf7695b0b98f516f6a9
Felipe Fernandez <felipe.fcordero@securitasdirect.es>
2023-10-17 11:07:40 +0200

    test: add second greet with fake repository

--- src/main/java/com/example/GreetingService.java
+++ src/main/java/com/example/GreetingService.java
@@ -3,8 +3,16 @@ package com.example;
 import lombok.Value;
 
 public class GreetingService {
+    private UserRepository userRepository;
+    public GreetingService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public Greet hello(String name) {
-        return new Greet("Hello " + name + ", good morning");
+        if(userRepository.findByName(name) == null){
+            return new Greet("Hello " + name + ", good morning");
+        }
+        return new Greet("I saw you before, " + name);
     }
 }
 
@@ -15,3 +23,15 @@ class Greet {
 
 //  { "message": "Hello...." }
 //
+
+class UserRepository{
+
+    public User findByName(String name) {
+        throw new RuntimeException("NOT IMPLEMENTED YET");
+    }
+}
+
+@Value
+class User{
+    String name;
+}
--- src/test/java/com/example/GreetingServiceTest.java
+++ src/test/java/com/example/GreetingServiceTest.java
@@ -3,15 +3,33 @@ package com.example;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 
 public class GreetingServiceTest {
 
+    UserRepository userRepository = mock(UserRepository.class);
+    GreetingService greetingService = new GreetingService(userRepository);
+
+
     @Test
     void should_say_hello() {
-        GreetingService greetingService = new GreetingService();
+        given(userRepository.findByName("Dorothy")).willReturn(null);
 
         assertThat(greetingService.hello("Dorothy"))
                 .isEqualTo(new Greet("Hello Dorothy, good morning"));
     }
+
+    @Test
+    void should_not_say_hello_two_times(){
+        given(userRepository.findByName("Dorothy")).willReturn(null)
+                .willReturn(new User("Dorothy"));
+
+        greetingService.hello("Dorothy");
+
+        assertThat(greetingService.hello("Dorothy"))
+                .isEqualTo(new Greet("I saw you before, Dorothy"));
+    }
+
 }
